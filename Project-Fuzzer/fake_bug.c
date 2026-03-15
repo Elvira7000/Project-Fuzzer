e101daefc35e1a71ae7cd749aac93955a933db6d
Elvira7000 <noumeuchoupinette@gmail.com>
2026-03-15 16:48:26 +0100

    fuzzer.c

---
+++ Project-Fuzzer/fake_bug.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("*** The program has crashed ***\n");
+    return 0;
+}
+
--- Project-Fuzzer/fuzzer.c
+++ Project-Fuzzer/fuzzer.c
@@ -74,35 +74,9 @@ unsigned int calculate_checksum(tar_t *header){
     return check;
 }
 
+    // Ecriture du contenu + padding
 
-int main(int argc, char* argv[])
-{
-    tar_t header;
-
-    memcpy(header.magic, TMAGIC, TMAGLEN);
-    memcpy(header.version, TVERSION, TVERSLEN);
-
-    unsigned long size_bytes = 512;
-
-    if (argc < 2){
-        fprintf(stderr, "Usage: %s <extracteur>\n", argv[0]);
-        return 1;
-    }
-    char *extracteur = argv[1];
-
-
-    init_header(&header, "Elvira.txt", '0', 1000);    // Créer un header
-    calculate_checksum(&header);
-
-
-    FILE *f = fopen("archive.tar", "wb");
-    if (f == NULL){
-        fprintf(stderr, "Erreur: impossible de créer archive.tar\n");
-        return 1;
-    }
-    fwrite(&header, 1, 512, f);
-
-    // Ecriture du contenu et padding 
+void write_content(FILE *f, unsigned long size_bytes) {
     if (size_bytes > 0){
         char *content = malloc(size_bytes);
         memset(content, 'A', size_bytes);
@@ -113,9 +87,284 @@ int main(int argc, char* argv[])
         char zeros[512];
         memset(zeros, 0, sizeof(zeros));
         fwrite(zeros, 1, padding, f);
+         
+    }
+    
+}
 
+
+
+// Test de crash
+
+int test_archive(const char *extracteur, const char *nom_archive) {
+    char cmd[256];
+    snprintf(cmd, sizeof(cmd), "%s %s",extracteur, nom_archive);
+
+    FILE *pipe = popen(cmd, "r");  
+    if (pipe == NULL) return 0;
+
+    char ligne[256];
+    int crash = 0;
+    while (fgets(ligne, sizeof(ligne), pipe) != NULL) {  
+        if (strstr(ligne, "*** The program has crashed ***")) {  
+            crash = 1;
+        }
     }
 
+    pclose(pipe);
+    return crash;
+
+}
+
+// save crashing
+
+void save_crashing(const char *message){
+    FILE *crashing = fopen("crashing", "a");
+    if (crashing != NULL){
+        fprintf(crashing, "%s\n", message);
+        fclose(crashing);
+    }
+}
+
+// Generation d'archive multifichiers
+
+void generate_archive(const char *filename, int N){
+    FILE *f = fopen("archive.tar", "wb");
+    if (f == NULL) return ;
+
+    tar_t header;
+    for (int i = 0; i < 5; i++) {
+        unsigned long size_bytes = (i % 2 == 0) ? 1000 : 0;
+        char nom[100];
+        snprintf(nom, sizeof(nom), "fichier%d.txt", i);
+        init_header(&header, nom, '0', size_bytes);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        write_content(f, size_bytes);
+    }
+
+    char fin[1024];
+    memset(fin, 0, sizeof(fin));
+    fwrite(fin, 1, 1024, f);
+    fclose(f);
+
+}
+
+// Fuzzing structure
+
+void fuzz_structure(const char *extracteur, int *crash_count){
+    tar_t header;
+
+    // Cas 1: fausse taille
+    {
+        FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return ;
+
+        init_header(&header, "fichier.txt", '0', 1000);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        // pas de contenu
+        char fin[1024];
+        memset(fin, 0, sizeof(fin));
+        fwrite(fin, 1, 1024, f);
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - fausse taille!\n");
+            rename("archive.tar", "success_structureB1.tar");
+            save_crashing("Crash - fausse taille");
+            crash_count++;
+        }
+    }
+
+    // 2e cas : Ecrire moins que prévu 
+
+    {
+        FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return ;
+
+        init_header(&header, "fichier.txt", '0', 1000);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        char petit[10];
+        memset(petit, 'A', sizeof(petit));
+        fwrite(petit, 1, 10, f);
+        char fin[1024];
+        memset(fin, 0, sizeof(fin));
+        fwrite(fin, 1, 1024, f);
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - moins que prévu!\n");
+            rename("archive.tar", "success_structureB2.tar");
+            save_crashing("Crash - moins que prévu");
+            crash_count++;
+        }
+    }
+
+    // 3e cas: Taille 0 mais données parasites
+
+    {
+        FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return ;
+
+        init_header(&header, "fichier.txt", '0', 0);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        char parasite[512];
+        memset(parasite, 'A', sizeof(parasite));
+        fwrite(parasite, 1, 10, f);
+        char fin[1024];
+        memset(fin, 0, sizeof(fin));
+        fwrite(fin, 1, 1024, f);
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - données parasites!\n");
+            rename("archive.tar", "success_structureB3.tar");
+            save_crashing("Crash - données parasites");
+            crash_count++;
+        }
+    }
+
+// Test : Fichiers sans fin
+
+    //Cas 1 : arret brutal
+
+    {
+        FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return ;
+
+        init_header(&header, "fichier.txt", '0', 512);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        write_content(f, 512);
+        // pas de bloc de fin
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - arret brutal!\n");
+            rename("archive.tar", "success_structureC1.tar");
+            save_crashing("Crash - arret brutal");
+            crash_count++;
+        }
+    }
+
+    // Cas 2 : un seul bloc de fin
+    {
+        FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return ;
+
+        init_header(&header, "fichier.txt", '0', 512);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+        write_content(f, 512);
+        char fin[512];
+        memset(fin, 0, sizeof(fin));
+        fwrite(fin, 1, 512, f);
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - un seul bloc de fin!\n");
+            rename("archive.tar", "success_structureC2.tar");
+            save_crashing("Crash - un seul bloc de fin");
+            crash_count++;
+        }
+    }
+
+// Cas : 02 headers vides consécutifs
+    {
+         FILE *f = fopen("archive.tar", "wb");
+        if (f == NULL) return;
+
+    // Premier header vide
+        init_header(&header, "fichier1.txt", '0', 0);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+    // pas de données
+
+    // Deuxième header vide immédiatement après
+        init_header(&header, "fichier2.txt", '0', 0);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+    // pas de données
+
+        char fin[1024];
+        memset(fin, 0, sizeof(fin));
+        fwrite(fin, 1, 1024, f);
+        fclose(f);
+
+        if (test_archive(extracteur, "archive.tar")) {
+            printf("CRASH - deux headers vides consécutifs!\n");
+            rename("archive.tar", "success_strategieC3.tar");
+            save_crashing("Crash - deux headers vides consécutifs");
+            (*crash_count)++;
+        }
+    }
+}
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2){
+        fprintf(stderr, "Usage: %s <extracteur>\n", argv[0]);
+        return 1;
+    }
+    char *extracteur = argv[1];
+    tar_t header;
+    int crash_count = 0;
+
+// Test archives multifichiers : 100 fichiers
+
+generate_archive("archive.tar", 100);
+if (test_archive(extracteur, "archive.tar")) {
+    printf("CRASH  - 100 fichiers!\n");
+    rename("archive.tar", "success_crash_100.tar");
+    save_crashing("Crash - 100 fichiers");
+    crash_count++;
+}
+
+// Test archives multifichiers : 1000 fichiers
+
+generate_archive("archive.tar", 1000);
+if (test_archive(extracteur, "archive.tar")) {
+    printf("CRASH  - 1000 fichiers!\n");
+    rename("archive.tar", "success_crash_1000.tar");
+    save_crashing("Crash - 1000 fichiers");
+    crash_count++;
+}
+   
+// fuzzing de la structure
+
+fuzz_structure(extracteur, &crash_count);
+
+
+printf("Total crashs détectés: %d\n", crash_count);
+    return 0;
+
+
+    /*unsigned long size_bytes = 512;
+
+    init_header(&header, "Elvira.txt", '0', size_bytes);    // Créer un header
+    calculate_checksum(&header);
+
+    int N = 5;
+    FILE *f = fopen("archive.tar", "wb");
+    if (f == NULL){
+        fprintf(stderr, "Erreur: impossible de créer archive.tar\n");
+        return 1;
+    }
+    for (int i = 0; i < N; i++){
+        unsigned long size_bytes = (i % 2 == 0) ? 1000 : 0;
+        char nom[100];
+        snprintf(nom, sizeof(nom), "fichier%d.txt", i);
+        init_header(&header, nom, '0', size_bytes);
+        calculate_checksum(&header);
+        fwrite(&header, 1, 512, f);
+
+// Ecriture du contenu et padding 
+               
+
+    }
     // Bloc de fin
     char zeros[1024];
     memset(zeros, 0, sizeof(zeros));
@@ -123,23 +372,8 @@ int main(int argc, char* argv[])
 
     fclose(f);   //fermeture du fichier
 
-    char cmd[256];
-    snprintf(cmd, sizeof(cmd), "%s archive.tar",extracteur);
 
-     FILE *pipe = popen(cmd, "r");  // 1. Exécution
-    if (pipe == NULL) {
-        fprintf(stderr, "Erreur: impossible de lancer l'extracteur\n");
-        return 1;
-    }
 
-    char ligne[256];
-    while (fgets(ligne, sizeof(ligne), pipe) != NULL) {  // 2. Lecture ligne par ligne
-        if (strstr(ligne, "*** The program has crashed ***")) {  // 3. Analyse
-            printf("CRASH DÉTECTÉ !\n");
-        }
-    }
-
-    pclose(pipe);  // 4. Fermeture
 
     // Ecriture du contenu de l'archive
 
@@ -162,7 +396,7 @@ int main(int argc, char* argv[])
     // Ecrire dans test.tar
     
     
-    // printf("archive.tar créé !\n");
+    // printf("archive.tar créé !\n"); */
 
 
 
